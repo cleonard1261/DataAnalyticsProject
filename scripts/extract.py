a720cc5a29486e873d8233fa88cc22317a3b4756
Chad Leonard <chadleonard@CWL2.local>
2014-04-05 17:58:46 -0400

    added groupby to extract.py

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,12 +1,27 @@
 #!/usr/bin/python
 
 
-
+from  dateutil import parser
 import pandas as pd
 
 df = pd.read_csv('../data/SRC__from03_06_2014__to03_06_2014.csv')
 
-
 df['row_ind'] = 1
 
-print pd.columns
+x = list(df['SERVICEORDERDATE'])
+
+
+for y in x:
+	dt = str(parser.parse(y).date())
+
+df['date_key'] = dt.replace('-','')
+
+
+#print df.columns
+
+grouped = df.groupby(['SERVICECODE', 'date_key']).row_ind.sum()
+
+print grouped
+
+#x = df['SERVICEORDERDATE'][1:].split(' ',0)
+
